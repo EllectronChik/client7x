20139aef43febaad1611c3c86e727e616a47fa04
ellectronchik <electronchik04@mail.ru>
2023-10-04 22:08:59 +0300

    Added player interface

---
+++ src/assets/images/models/IPlayer.ts
@@ -0,0 +1,10 @@
+export interface IPlayer {
+    id: number;
+    username: string;
+    avatar: string;
+    mmr: number;
+    wins: number;
+    total_games: number;
+    race: number;
+    team: number;
+}
