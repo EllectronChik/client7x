79ced941a4407de936742f620177953bfa886cdd
ellectronchik <electronchik04@mail.ru>
2023-10-04 22:10:20 +0300

    changed button7x comp to link7x and refactor it

--- src/components/UI/Button7x/Button7x.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button7x = () => {
-  return (
-    <div>Button7x</div>
-  )
-}
-
-export default Button7x;
---
+++ src/components/UI/Link7x/Link7x.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './Link7x.module.scss';
+
+interface Button7xProps {
+  to: string
+  children?: React.ReactNode
+}
+
+const Link7x: React.FC<Button7xProps> = ({to, children}) => {
+  return (
+    <Link to={to}>
+      <div className={`${classes.button} ${classes.v8}`}>
+        <span className={classes.label}>
+          {children}
+        </span>
+        <span className={classes.icon}>
+        <span></span>
+      </span>
+    </div>
+  </Link>
+  )
+}
+
+export default Link7x;
