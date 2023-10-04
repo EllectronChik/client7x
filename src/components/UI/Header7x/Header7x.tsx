20ad0ceda1540237c44d5117fa1551f8938975a2
ellectronchik <electronchik04@mail.ru>
2023-10-04 22:10:55 +0300

    finished header

--- src/components/UI/Header7x/Header7x.tsx
+++ src/components/UI/Header7x/Header7x.tsx
@@ -2,26 +2,30 @@ import React from 'react'
 import classes from './Header7x.module.scss';
 import { Link } from 'react-router-dom';
 import logo from '@assets/images/favicon.svg';
+import Link7x from '../Link7x/Link7x';
+import DoubleText from '../DoubleText/DoubleText';
 
 
 const Header7x = () => {
   return (
+    <div>
     <header className={classes.header}>
-      <span className={classes.left_border}></span>
       <div className={classes.container}>
         <Link className={classes.logo_link} to='/'>
           <img className={classes.logo} src={logo} alt="Logo_7x" />
         </Link>
+        <DoubleText className={classes.title} text='Team-League'/>
         <nav>
           <ul className={classes.nav}>
-            <li className={classes.nav_item}><Link to='/arhive'>Arhive</Link></li>
-            <li className={classes.nav_item}><Link to='/settings'>Settings</Link></li>
+            <li className={classes.nav_item}><Link7x to='/statistics'>Statistic</Link7x></li>
+            <li className={classes.nav_item}><Link7x to='/arhive'>Arhive</Link7x></li>
+            <li className={classes.nav_item}><Link7x to='/login'>Join</Link7x></li>
           </ul>
         </nav>
       </div>
-      <span className={classes.right_border}></span>
-      <span className={classes.bottom_border}></span>
     </header>
+    <div className={classes.line}></div>
+    </div>
   )
 }
 
