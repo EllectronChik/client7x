6cb13d12602a76bba54aef15766c5a08ee26b928
ellectronchik <electronchik04@mail.ru>
2023-10-04 22:10:38 +0300

    added doubletext component

---
+++ src/components/UI/DoubleText/DoubleText.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './DoubleText.module.scss';
+
+interface DoubleTextProps {
+    className?: string;
+    text: string;
+}
+
+const DoubleText: React.FC<DoubleTextProps> = ({text, className}) => {
+    const textArray = [...text];
+    console.log(textArray);
+    return (
+        <div className={`${classes.double_text} ${className}`}>
+            {textArray.map((letter, index) => (
+                <span data-title={letter} key={index}>{letter}</span>
+            ))}
+        </div>
+    );
+}
+
+export default DoubleText;
