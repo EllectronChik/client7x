6a0f1720f33a2619888ff984ebbd2732e9325147
ellectronchik <electronchik04@mail.ru>
2023-10-05 21:01:50 +0300

    added isAuth control

--- src/components/UI/Header7x/Header7x.tsx
+++ src/components/UI/Header7x/Header7x.tsx
@@ -4,9 +4,22 @@ import { Link } from 'react-router-dom';
 import logo from '@assets/images/favicon.svg';
 import Link7x from '../Link7x/Link7x';
 import DoubleText from '../DoubleText/DoubleText';
+import { useCookies } from 'react-cookie';
+import { UsersApi } from 'services/UserService';
+import Button7x from '../Button7x/Button7x';
 
 
-const Header7x = () => {
+const Header7x: React.FC = () => {
+  const [token, setToken] = useCookies(['token']);
+  const [userId, setUserId] = useCookies(['userId']);
+  const [logoutUser, {}] = UsersApi.useLogoutUserMutation();
+
+  const logout = async () => {
+    await logoutUser(token.token);
+    setToken('token', '', { expires: new Date(0) });
+    setUserId('userId', '', { expires: new Date(0) });
+  }
+
   return (
     <div>
     <header className={classes.header}>
@@ -19,7 +32,11 @@ const Header7x = () => {
           <ul className={classes.nav}>
             <li className={classes.nav_item}><Link7x to='/statistics'>Statistic</Link7x></li>
             <li className={classes.nav_item}><Link7x to='/arhive'>Arhive</Link7x></li>
+            {token.token ?
+            <li className={classes.nav_item}><Button7x onClick={() => logout()}>Logout</Button7x></li>
+            :
             <li className={classes.nav_item}><Link7x to='/login'>Join</Link7x></li>
+            }
           </ul>
         </nav>
       </div>
