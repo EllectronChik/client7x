e956b52d80520763d89f0e58c56b6688af3fcdb6
ellectronchik <electronchik04@mail.ru>
2023-10-05 21:04:49 +0300

    added UsersApi

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { MatchesApi } from '../services/MatchesService';
+import { UsersApi } from 'services/UserService';
 
 const rootReducer = combineReducers({
-    [MatchesApi.reducerPath]: MatchesApi.reducer
+    [MatchesApi.reducerPath]: MatchesApi.reducer,
+    [UsersApi.reducerPath]: UsersApi.reducer,
 })
 
 export const setupStore = () => {
     return configureStore({
         reducer: rootReducer,
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(MatchesApi.middleware)
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+        .concat(MatchesApi.middleware)
+        .concat(UsersApi.middleware)
     })
         
 }
