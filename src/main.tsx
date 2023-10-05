a81a709de44cb60a80af2dda461c8dc543086b14
ellectronchik <electronchik04@mail.ru>
2023-10-05 20:59:22 +0300

    added redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <Provider store={setupStore()}>
+        <App />
+    </Provider>
 )
