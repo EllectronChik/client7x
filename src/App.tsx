9e0fec288c4ee9e71dabe5f47fe2c5a3b362cf52
ellectronchik <electronchik04@mail.ru>
2023-10-05 20:59:07 +0300

    deleted retitling by script

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React, { useEffect } from 'react';
 import './App.scss';
 import AppRouter from './components/AppRouter';
 
 function App() {
-  useEffect(() => {
-    document.title = '7x League';
-  }, [])
   return (
     <div className="App">
       <AppRouter />
