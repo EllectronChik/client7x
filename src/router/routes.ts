aaa328a7563880214ebef979fa9e550d508a7a21
ellectronchik <electronchik04@mail.ru>
2023-10-05 21:03:35 +0300

    login page routs

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,9 @@
 import StartPage from '../pages/StartPage';
 import Arhive from './../pages/Arhive';
+import Login from '../pages/Login/Login';
 
 export const PublicRoutes = [
     {path: '/arhive', Element: Arhive},
+    {path: '/login', Element: Login},
     {path: '/', Element: StartPage},
 ]
