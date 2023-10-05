7ee4b4aadffe39889f9741b8e70759220d5e9724
ellectronchik <electronchik04@mail.ru>
2023-10-05 21:00:39 +0300

    same look as link7x but is button

---
+++ src/components/UI/Button7x/Button7x.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './Button7x.module.scss';
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+
+interface Button7xProps {
+    className?: string;
+    type?: "button" | "submit" | "reset" | undefined;
+    children?: React.ReactNode;
+    onClick?: () => void;
+}
+
+const Button7x: React.FC<Button7xProps> = ({type, children, className, onClick}) => {
+  return (
+  <button className={className} type={type} onClick={onClick}>
+    <div className={`${classes.button} ${classes.v8}`}>
+    <span className={classes.label}>
+      {children}
+    </span>
+    <span className={classes.icon}>
+    <span></span>
+  </span>
+</div>
+</button>
+  )
+}
+
+export default Button7x
