c83f809dd74f7827ffa43823cb27480458d38ee9
ellectronchik <electronchik04@mail.ru>
2023-10-06 21:17:37 +0300

    Added account page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,11 @@
 import StartPage from '../pages/StartPage';
 import Arhive from './../pages/Arhive';
 import Login from '../pages/Login/Login';
+import Account from './../pages/Account/Account';
 
 export const PublicRoutes = [
     {path: '/arhive', Element: Arhive},
     {path: '/login', Element: Login},
+    {path: '/account', Element: Account},
     {path: '/', Element: StartPage},
 ]
