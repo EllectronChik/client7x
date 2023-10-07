1dbe909ce12e4993d55f979072bcc3463367a949
ellectronchik <electronchik04@mail.ru>
2023-10-07 15:27:12 +0300

    new Hook

---
+++ src/hooks/useLogoutUser.ts
@@ -0,0 +1,21 @@
+import { useCookies } from "react-cookie";
+import { UsersApi } from "services/UserService";
+
+
+export const useLogoutUser = () => {
+    const [cookie, setСookie] = useCookies(['token', 'userId']);
+    const [logoutUser, {}] = UsersApi.useLogoutUserMutation();
+  
+    const logout = async () => {
+      try {
+        await logoutUser(cookie.token);
+        setСookie('token', '', { expires: new Date(0) });
+        setСookie('userId', '', { expires: new Date(0) });
+      } catch (error) {
+        console.error("Ошибка при выходе из системы:", error);
+      }
+    };
+  
+    return logout; 
+  };
+  
