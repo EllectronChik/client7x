94ff7b5bfd62ab1dc10c3750869f597c789242ba
ellectronchik <electronchik04@mail.ru>
2023-10-07 21:07:44 +0300

    added clanapi

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { MatchesApi } from '../services/MatchesService';
 import { UsersApi } from 'services/UserService';
 import { StatusApi } from 'services/StatusService';
+import { ClanApi } from 'services/ClanService';
 
 const rootReducer = combineReducers({
     [MatchesApi.reducerPath]: MatchesApi.reducer,
     [UsersApi.reducerPath]: UsersApi.reducer,
     [StatusApi.reducerPath]: StatusApi.reducer,
+    [ClanApi.reducerPath]: ClanApi.reducer
 })
 
 export const setupStore = () => {
@@ -15,7 +17,8 @@ export const setupStore = () => {
         middleware: (getDefaultMiddleware) => getDefaultMiddleware()
         .concat(MatchesApi.middleware)
         .concat(UsersApi.middleware)
-        .concat(StatusApi.middleware),
+        .concat(StatusApi.middleware)
+        .concat(ClanApi.middleware)
 
     })
         
