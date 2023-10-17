dd5a97a4f7119e220368acc3680b8ab73c70fd10
ellectronchik <electronchik04@mail.ru>
2023-10-17 22:55:08 +0300

    added clanResApi

---
+++ src/services/ClanResourcesService.ts
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { IResorce } from "models/IResorce";
+
+
+export const ClanResourcesApi = createApi({
+    reducerPath: "clanResourcesApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_API_URL,
+    }),
+    tagTypes: ["clanResources"],
+    endpoints: (builder) => ({
+        fetchClanResources: builder.query<IResorce[], number>({
+            query: (id) => ({
+                url: `/team_resources/${id}/`,
+                method: "GET",
+            })
+        }),
+        postClanResource: builder.mutation<void, {resource: IResorce, token: string}>({
+            query: ({resource, token}) => ({
+                url: `/team_resources/`,
+                method: "POST",
+                body: resource,
+                headers: {
+                    Authorization: `Token ${token}`
+                }         
+            })
+        })
+    })
+})
