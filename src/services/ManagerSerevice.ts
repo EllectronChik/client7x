88c29c97c8ed31cabc7207ccb66bca30d1424162
ellectronchik <electronchik04@mail.ru>
2023-10-17 22:55:25 +0300

    added managerApi

---
+++ src/services/ManagerSerevice.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { IManager } from "models/IManager";
+
+
+export const ManagerApi = createApi({
+    reducerPath: "managerApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_API_URL,
+    }),
+    tagTypes: ["manager"],
+    endpoints: (builder) => ({
+        postManager: builder.mutation<void, {manager: IManager, token: string}>({
+            query: ({manager, token}) => ({
+                url: `/managers/`,
+                method: "POST",
+                body: manager,
+                headers: {
+                    Authorization: `Token ${token}`
+                }         
+            })
+        })
+    })
+})
