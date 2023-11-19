15c67defca5bc914f6b435bc40d7811945851869
EllectronChik <electronchik04@mail.ru>
2023-11-19 18:08:19 +0300

    added localization logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.scss';
-import AppRouter from './components/AppRouter';
+import AppRouter from './components/AppRouter/AppRouter';
+import { IntlProvider } from 'react-intl';
+import { LOCALES } from 'i18n/locales';
+import { MESSAGES } from 'i18n/messages';
+import { useCookies } from 'react-cookie';
+
 
 function App() {
+  const [cookies] = useCookies(['locale']);
+  let locale = window.navigator.language || LOCALES.ENGLISH;
+  if (cookies.locale) {
+    locale = cookies.locale;
+  }
   return (
     <div className="App">
+      <IntlProvider
+        messages={MESSAGES[locale]}
+        locale={locale}
+        defaultLocale={LOCALES.ENGLISH}>
       <AppRouter />
+      </IntlProvider>
     </div>
   );
 }
