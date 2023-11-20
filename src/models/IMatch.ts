cec92accb5e74f50f53cef6031020b13cde99c7b
EllectronChik <electronchik04@mail.ru>
2023-11-20 22:12:44 +0300

    channged interface

--- src/models/IMatch.ts
+++ src/models/IMatch.ts
@@ -1,9 +1,8 @@
 export interface IMatch {
     id: number;
-    season: number;
-    stage: number;
-    match_start_time: Date;
     player_1_id: number;
     player_2_id: number;
-    is_finished: boolean;
+    winner: boolean;
+    tournament: number;
+    map: string;
 }
