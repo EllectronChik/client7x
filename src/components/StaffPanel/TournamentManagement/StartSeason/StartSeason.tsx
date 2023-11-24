ff5f49f7ad982fd08acc58a7586e6fb75c4a2860
EllectronChik <electronchik04@mail.ru>
2023-11-24 22:21:41 +0300

    destructurized + added TZ note

--- src/components/StaffPanel/TournamentManagement/StartSeason/StartSeason.tsx
+++ src/components/StaffPanel/TournamentManagement/StartSeason/StartSeason.tsx
@@ -6,67 +6,58 @@ import classes from './StartSeason.module.scss'
 import { FormattedMessage } from 'react-intl';
 import Input7x from 'components/UI/Input7x/Input7x';
 import Button7x from 'components/UI/Button7x/Button7x';
-import Loader7x from 'components/UI/Loader7x/Loader7x';
 
-const StartSeason: React.FC = () => {
+interface StartSeasonProps extends React.HTMLProps<HTMLFormElement> {
+    setSeasonStarted: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const StartSeason: React.FC<StartSeasonProps> = ({...props}) => {
     const [startSeason, {error: seasonStartError}] = SeasonApi.useStartSeasonMutation();
-    const {data: currentSeason, isLoading: currentSeasonLoading} = SeasonApi.useFetchCurrentSeasonQuery();
     const {data: lastSeasonNumber, } = SeasonApi.useFetchLastSeasonNumberQuery();
     const [cookies] = useCookies(['token']);
-    const [seasonStarted, setSeasonStarted] = useState<boolean>(false);
     const [seasonStartErrorState, setSeasonStartErrorState] = useState<boolean>(false);
+    const [timeZoneOffset, ] = useState<number>(- new Date().getTimezoneOffset() / 60);
+    const [timeZoneOffsetString, setTimeZoneOffsetString] = useState<string>(String(timeZoneOffset));
 
     useEffect(() => {
-        if(currentSeason) {
-            setSeasonStarted(true);
+        if (timeZoneOffset < 0) {
+            setTimeZoneOffsetString(`- ${timeZoneOffsetString}`);
+        } else {
+            setTimeZoneOffsetString(`+ ${timeZoneOffsetString}`);
         }
-    }, [currentSeason])
+    }, [timeZoneOffset])    
     
   return (
-    <div>
-        {seasonStarted ? 
-        <div>
-            SEASON  STARTED
-        </div>
-        :
-        <div>
-            {currentSeasonLoading 
-            ? 
-            <Loader7x />
-            :
-            <form className={classes.form} onSubmit={(e) => {
-                e.preventDefault();
-                if (!e.currentTarget.start_time.value) {
-                    setSeasonStartErrorState(true);
-                    return;
-                }
-                const isoTime = new Date(e.currentTarget.start_time.value).toISOString();
-                startSeason({
-                    token: cookies.token,
-                    season: {
-                        number: lastSeasonNumber ? lastSeasonNumber + 1 : 1,
-                        start_datetime: isoTime,
-                        is_finished: false,
-                        can_register: true
-                    }
-                });
-                setSeasonStarted(true);
-            }}>
-                <FormattedMessage id='no_seasons' />
-                {(seasonStartError || seasonStartErrorState) && <div className={classes.error}><img className={classes.errorIcon} src={important} alt="ERROR: " />
-                <FormattedMessage id='required_field' /></div>}
-                <div className={classes.form_content}>
-                    <label htmlFor='start_time'><FormattedMessage id='start_time' /></label>
-                    <Input7x id='start_time' type='datetime-local' />
-                </div>
-                <div>
-                    <Button7x type='submit'><FormattedMessage id='start_season' /></Button7x>
-                </div>
-            </form>
-            }
-        </div>
+    <form className={classes.form} onSubmit={(e) => {
+        e.preventDefault();
+        if (!e.currentTarget.start_time.value) {
+            setSeasonStartErrorState(true);
+            return;
         }
-    </div>
+        const isoTime = new Date(e.currentTarget.start_time.value).toISOString();
+        startSeason({
+            token: cookies.token,
+            season: {
+                number: lastSeasonNumber ? lastSeasonNumber + 1 : 1,
+                start_datetime: isoTime,
+                is_finished: false,
+                can_register: true
+            }
+        });
+        
+        props.setSeasonStarted(true);
+    }}>
+        <FormattedMessage id='no_seasons' />
+        {(seasonStartError || seasonStartErrorState) && <div className={classes.error}><img className={classes.errorIcon} src={important} alt="ERROR: " />
+        <FormattedMessage id='required_field' /></div>}
+        <div className={classes.form_content}>
+            <label htmlFor='start_time'><FormattedMessage id='start_time' /> (UTC {timeZoneOffsetString}) </label>
+            <Input7x id='start_time' type='datetime-local' />
+        </div>
+        <div>
+            <Button7x type='submit'><FormattedMessage id='start_season' /></Button7x>
+        </div>
+    </form>
   )
 }
 
