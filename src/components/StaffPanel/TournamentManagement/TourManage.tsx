83c31764821a97c2a48087cde356b9ac2058348a
EllectronChik <electronchik04@mail.ru>
2023-11-24 22:20:11 +0300

    added start or manage season logic

--- src/components/StaffPanel/TournamentManagement/TourManage.tsx
+++ src/components/StaffPanel/TournamentManagement/TourManage.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import Loader7x from 'components/UI/Loader7x/Loader7x';
+import React, { useEffect, useState } from 'react';
+import StartSeason from './StartSeason/StartSeason';
+import { SeasonApi } from 'services/SeasonService';
+import StartedSeasonManage from './StartedSeasonManage/StartedSeasonManage';
 
 const TourManage: React.FC = () => {
+  const {data: currentSeason, isLoading: currentSeasonLoading} = SeasonApi.useFetchCurrentSeasonQuery();
+  const [seasonStarted, setSeasonStarted] = useState<boolean>(false);
+
+
+  useEffect(() => {
+    if(currentSeason) {
+        setSeasonStarted(true);
+    }
+}, [currentSeason])
+
   return (
     <div>
-
+      {currentSeasonLoading ?
+       <Loader7x />
+      : (!seasonStarted && !currentSeason) ? <div>
+        <StartSeason setSeasonStarted={setSeasonStarted} /> 
+      </div>
+      : <StartedSeasonManage />
+      }
     </div>
   )
 }
