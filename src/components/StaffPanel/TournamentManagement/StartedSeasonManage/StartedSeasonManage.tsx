75b4ed3bcb02a38e6dc1fc36f3f0873b5bb65d29
EllectronChik <electronchik04@mail.ru>
2023-11-25 22:02:03 +0300

    finished

--- src/components/StaffPanel/TournamentManagement/StartedSeasonManage/StartedSeasonManage.tsx
+++ src/components/StaffPanel/TournamentManagement/StartedSeasonManage/StartedSeasonManage.tsx
@@ -1,19 +1,25 @@
-import { ISeason } from 'models/ISeason';
+import Button7x from 'components/UI/Button7x/Button7x';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react'
 import { useCookies } from 'react-cookie';
 import { SeasonApi } from 'services/SeasonService';
+import classes from './StartedSeasonManage.module.scss';
+import { FormattedMessage } from 'react-intl';
 
+interface StartedSeasonManageProps {
+    setSeasonStarted: React.Dispatch<React.SetStateAction<boolean | undefined>>;
+    timeZoneOffsetString: string;
+}
 
-const StartedSeasonManage: React.FC = () => {
-    const {data: currentSeason, isLoading: currentSeasonLoading} = SeasonApi.useFetchCurrentSeasonQuery();
-    const [cookies, setCookies] = useCookies(['token']);
+const StartedSeasonManage: React.FC<StartedSeasonManageProps> = ({...props}) => {
+    const {data: currentSeason, } = SeasonApi.useFetchCurrentSeasonQuery();
+    const [cookies, ] = useCookies(['token']);
     const [changeTime, {}] = SeasonApi.useChangeDatetimeMutation();
     const [changeIsFinished, {}] = SeasonApi.useChangeIsFinishedMutation();
     const [changeCanRegister, {}] = SeasonApi.useChangeCanRegisterMutation();
     const [localTime, setLocalTime] = useState<string>('');
+    const [askForFinished, setAskForFinished] = useState<boolean>(false);
     let setDateTimeout: NodeJS.Timeout | null = null;
-    let setIsFinishedTimeout: NodeJS.Timeout | null = null;
     let setCanRegisterTimeout: NodeJS.Timeout | null = null;
 
     useEffect(() => {
@@ -23,48 +29,58 @@ const StartedSeasonManage: React.FC = () => {
     }, [currentSeason])
 
   return (
-    <form>
-        <label htmlFor="datetime">Tournament Start Time</label>
-        <input type="datetime-local" defaultValue={localTime} onChange={(e) => {
-            if (setDateTimeout) {
-                clearTimeout(setDateTimeout);
-            }
-            setDateTimeout = setTimeout(() => {
-                changeTime({
-                    token: cookies.token,
-                    datetime: new Date(e.target.value).toISOString(),
-                    season: currentSeason ? currentSeason.number : 0 
-            });
-        }, 3000)}} />
-        <label htmlFor="can_register">Can Register</label>
-        <input id="can_register" type="checkbox" defaultChecked={currentSeason?.can_register} onChange={(e) => {
-            if (setCanRegisterTimeout) {
-                clearTimeout(setCanRegisterTimeout);
-            }
-            setCanRegisterTimeout = setTimeout(() => {
-                changeCanRegister({
-                    token: cookies.token,
-                    can_register: e.target.checked,
-                    season: currentSeason ? currentSeason.number : 0 
+    <div className={`${classes.form} ${askForFinished ? classes.formAskForFinished : ''}`}>
+        <h2><FormattedMessage id='manage_started_season' values={{season: currentSeason?.number}} /></h2>
+        <div>
+            <label className={classes.label} htmlFor="datetime"><FormattedMessage id='tournament_start' />(UTC {props.timeZoneOffsetString}): </label>
+            <input id="datetime" className={classes.input} type="datetime-local" defaultValue={localTime} onChange={(e) => {
+                if (setDateTimeout) {
+                    clearTimeout(setDateTimeout);
+                }
+                setDateTimeout = setTimeout(() => {
+                    changeTime({
+                        token: cookies.token,
+                        datetime: new Date(e.target.value).toISOString(),
+                        season: currentSeason ? currentSeason.number : 0 
                 });
-        }, 3000)}} />
-        <label htmlFor="is_finished">Is Finished</label>
-        <input id="is_finished" type="checkbox" defaultChecked={currentSeason?.is_finished} onChange={(e) => {
-            if (setIsFinishedTimeout) {
-                clearTimeout(setIsFinishedTimeout);
-            }
-            setIsFinishedTimeout = setTimeout(() => {
-                changeIsFinished({
-                    token: cookies.token,
-                    is_finished: e.target.checked,
-                    season: currentSeason ? currentSeason.number : 0 
-                });
-
-
-                // TODO: Add confirmation of tournament completion
-
-        }, 3000)}} />
-    </form>
+            }, 3000)}} />
+        </div>
+        <div className={classes.form_content}>
+            <label className={classes.label} htmlFor="can_register"><FormattedMessage id='open_registration' /></label>
+            <input className={classes.input} id="can_register" type="checkbox" defaultChecked={currentSeason?.can_register} onChange={(e) => {
+                if (setCanRegisterTimeout) {
+                    clearTimeout(setCanRegisterTimeout);
+                }
+                setCanRegisterTimeout = setTimeout(() => {
+                    changeCanRegister({
+                        token: cookies.token,
+                        can_register: e.target.checked,
+                        season: currentSeason ? currentSeason.number : 0 
+                    });
+            }, 3000)}} />
+        </div>
+        <Button7x className={classes.button} onClick={() => {
+            setAskForFinished(true);
+            }} ><FormattedMessage id='finish_tournament' /></Button7x>
+        {askForFinished && 
+        <div>
+            <p className={classes.confirm_finish}><FormattedMessage id='confirm_finish' /></p>
+            <div className={classes.buttons}>
+                <Button7x className={classes.button} onClick={() => {
+                    changeIsFinished({
+                        token: cookies.token,
+                        is_finished: true,
+                        season: currentSeason ? currentSeason.number : 0 
+                    });
+                    props.setSeasonStarted(false);
+                }}><FormattedMessage id='yes' /></Button7x>
+                <Button7x className={classes.button} onClick={() => {
+                    setAskForFinished(false);
+                }}><FormattedMessage id='no' /></Button7x>
+            </div>
+        </div>
+        }
+    </div>
   )
 }
 
