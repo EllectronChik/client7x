36f991d54f6c9fbe0d4c47f7e338f8b68a9afea0
EllectronChik <electronchik04@mail.ru>
2023-11-28 19:59:24 +0300

    added title change

--- src/components/StaffPanel/StaffPanel.tsx
+++ src/components/StaffPanel/StaffPanel.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TourManage from './TournamentManagement/TourManage';
-
+import { useIntl } from 'react-intl';
 
 const StaffPanel: React.FC = () => {
+  const intl = useIntl();
+
+  useEffect(() => {
+    document.title = intl.formatMessage({id: 'staff'})
+  }, [intl])
+
   return (
     <div>
       <TourManage />
