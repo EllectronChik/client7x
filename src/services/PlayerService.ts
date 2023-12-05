4e58b293d7df6556569aabdd88365a5e3ef163d3
EllectronChik <electronchik04@mail.ru>
2023-12-05 19:26:00 +0300

    added getRegForSeasonPlayers

--- src/services/PlayerService.ts
+++ src/services/PlayerService.ts
@@ -32,13 +32,13 @@ export const PlayerApi = createApi({
                 }
             })
         }),
-        postPlayerToSeason: builder.mutation<void, {player: IPlayer, token: string, season: number}>({
-            query: ({player, token, season}) => ({
+        postPlayerToSeason: builder.mutation<void, {player_id: number, token: string, season: number}>({
+            query: ({player_id, token, season}) => ({
                 url: `/player_to_tournament/`,
                 method: "POST",
                 body: {
-                    player: player,
-                    season: season
+                    player: player_id,
+                    season_number: season
                 },
                 headers: {
                     Authorization: `Token ${token}`
@@ -52,6 +52,12 @@ export const PlayerApi = createApi({
                 headers: {
                     Authorization: `Token ${token}`
                 }
+            })
+        }),
+        getRegForSeasonPlayers: builder.query<{player: number, season: number, user: number}[], {user: number, season: number | undefined}>({
+            query: ({user, season}) => ({
+                url: `/player_to_tournament/?season${season}&user=${user}`,
+                method: "GET",
             })
         })
     })
