beeb1c0893a967f739c47d5fa2990bda74e9625e
EllectronChik <electronchik04@mail.ru>
2023-12-11 23:20:10 +0300

    started logic

--- src/components/StaffPanel/TournamentManagement/GroupDistribution/GroupDistribution.tsx
+++ src/components/StaffPanel/TournamentManagement/GroupDistribution/GroupDistribution.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie';
+import { GroupApi } from 'services/GroupService'
+import classes from './GroupDistribution.module.scss'
+import { IClan } from 'models/IClan';
 
 
 const GroupDistribution: React.FC = () => {
+  const [cookies] = useCookies(['token']);
+  const {data: registredTeams} = GroupApi.useFetchRegistredTeamsQuery(cookies.token);
+  const {data: groups} = GroupApi.useFetchGroupsQuery(cookies.token);
+  const [ undistributedTeams, setUndistributedTeams ] = useState<IClan[]>([]);
+  const [ groupsTeams, setGroupsTeams ] = useState<IClan[]>([]);
+
+  useEffect(() => {
+    if (registredTeams && groups) { 
+      console.log('registredTeams', registredTeams);
+      console.log('groups', groups);      
+      registredTeams.forEach(team => {
+        groups.forEach(group => {
+          group.teams.forEach(teamGroup => {
+            if (teamGroup.id === team.id) {
+              setGroupsTeams([...groupsTeams, teamGroup]);
+            } else {
+              setUndistributedTeams([...undistributedTeams, teamGroup]);
+            }
+          })
+        })
+      })
+    }
+  }, [groups, registredTeams])
+
+  useEffect(() => {
+    console.log('undistributedTeams', undistributedTeams);
+  }, [undistributedTeams])
+
+  useEffect(() => {
+    console.log('groupsTeams', groupsTeams);
+  }, [groupsTeams])
 
   return (
     <div>
-      
+      <div className={classes.undistributedTeams}>
+        <p>NOT GROUPS</p>
+      </div>
     </div>
   )
 }
