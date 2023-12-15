58f33d29f1a27efcc3efe03607ccd09b7c94e2f1
EllectronChik <electronchik04@mail.ru>
2023-12-16 01:46:48 +0300

    created

---
+++ src/models/ITournament.ts
@@ -0,0 +1,9 @@
+export interface ITournament {
+    season?: number,
+    match_start_time: string,
+    is_finished?: boolean,
+    team_one: number,
+    team_two: number,
+    stage: number
+    group?: number,
+}
