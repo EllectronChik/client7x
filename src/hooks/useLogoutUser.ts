c5d8e2dd05fcf8c3ce6bb6e808b7e30241c9fb52
EllectronChik <electronchik04@mail.ru>
2024-01-28 01:07:54 +0300

    added reloading

--- src/hooks/useLogoutUser.ts
+++ src/hooks/useLogoutUser.ts
@@ -1,21 +1,16 @@
 import { useCookies } from "react-cookie";
 import { DeviceCntApi } from "services/DeviceCntService";
-import { returnToInitialState as AccountReturnToInitialState } from "store/reducers/AccountSlice";
-import { returnToInitialState as DragPlayerReturnToInitialState } from "store/reducers/DragPlayerSlice";
-import { useAppDispatch } from "./reduxHooks";
 
 export const useLogoutUser = () => {
     const [cookie, setCookie] = useCookies(['token', 'userId']);
     const [decreaseDevices, {}] = DeviceCntApi.usePatchDeviceCntMutation();
-    const dispatch = useAppDispatch();
   
     const logout = async () => {
       try {
         await decreaseDevices({token: cookie.token, action: 'decrease'});
         setCookie('token', '', { expires: new Date(0) });
         setCookie('userId', '', { expires: new Date(0) });
-        dispatch(AccountReturnToInitialState());
-        dispatch(DragPlayerReturnToInitialState());
+        window.location.reload();
       } catch (error) {
         console.error("Error when logging out of the system:", error);
       }
