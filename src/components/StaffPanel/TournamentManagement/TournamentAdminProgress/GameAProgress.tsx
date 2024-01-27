391f0f180c78441fdecf5e0e82abf7dd7e86c993
EllectronChik <electronchik04@mail.ru>
2024-01-28 01:03:00 +0300

    Destructurized  FC

---
+++ src/components/StaffPanel/TournamentManagement/TournamentAdminProgress/GameAProgress.tsx
@@ -0,0 +1,244 @@
+import { FC } from 'react'
+import deleteWhite from 'assets/images/techImages/delete_white.svg';
+import { Tooltip } from 'react-tooltip';
+import editWhite from 'assets/images/techImages/edit_white.svg';
+import { useCookies } from 'react-cookie';
+import {
+    selectMapNames,
+    setMapNames,
+    selectLocalTimes,
+    setLocalTimes,
+    selectMatchEdit,
+    setMatchEdit,
+    selectMatches
+           } from 'store/reducers/TournamentsAdminSlice';
+import { useAppDispatch, useAppSelector } from 'hooks/reduxHooks';
+import { TournamentApi } from 'services/TournamentService';
+import classes from './TournamentAdminProgress.module.scss';
+import { ITournamentAdmin } from 'models/ITournamentAdmin';
+import { IMatch } from 'models/IMatch';
+
+interface IProps {
+    tournament: ITournamentAdmin
+    tournamentsWebSocketRef: React.MutableRefObject<WebSocket | undefined>
+    matchesWebSocketsRef: React.MutableRefObject<{[key: number]: WebSocket}>
+    matchesWebSocketFunc: (tournament: ITournamentAdmin) => void,
+    deletedMatches: number[],
+    setDeletedMatches: React.Dispatch<React.SetStateAction<number[]>>
+}
+
+const GameAProgress: FC<IProps> = ({
+        tournament, 
+        tournamentsWebSocketRef, 
+        matchesWebSocketsRef, 
+        matchesWebSocketFunc, 
+        deletedMatches, 
+        setDeletedMatches}) => {
+    const [cookies] = useCookies(['token', 'userId']);
+    const dispatch = useAppDispatch();
+    const mapNames = useAppSelector(selectMapNames);
+    const localTimes = useAppSelector(selectLocalTimes);
+    const matchEdit = useAppSelector(selectMatchEdit);
+    const matches = useAppSelector(selectMatches);
+    const {data: playersInTeams} = TournamentApi.useFetchPlayerByTeamsQuery(cookies.token);
+
+    return <div className={classes.tournament} key={tournament.id}>
+    <div className={classes.tournamentInfo}>
+      <h3 className={`${tournament.winner === tournament.teamOne ? classes.winner : ''} ${classes.team}`}
+          onClick={() => {
+            if (tournament.winner !== tournament.teamOne && tournamentsWebSocketRef.current) {
+              tournamentsWebSocketRef.current.send(JSON.stringify({
+                action: 'update',
+                tournament_id: tournament.id,
+                field: 'winner',
+                value: tournament.teamOne
+              }))
+            }
+          }}>{tournament.teamOneName}</h3>
+      <h3>{tournament.teamOneWins}</h3>
+      <h3>:</h3>
+      <h3>{tournament.teamTwoWins}</h3>
+      <h3 className={`${tournament.winner === tournament.teamTwo ? classes.winner : ''} ${classes.team}`}
+          onClick={() => {
+            if (tournament.winner !== tournament.teamTwo && tournamentsWebSocketRef.current) {
+              tournamentsWebSocketRef.current.send(JSON.stringify({
+                action: 'update',
+                tournament_id: tournament.id,
+                field: 'winner',
+                value: tournament.teamTwo
+              }))
+            }
+          }}>{tournament.teamTwoName}</h3>
+    </div>
+    <div className={classes.techInputContainer}>
+      {localTimes[tournament.id] && <input className={classes.dateInput} type="datetime-local" value={localTimes[tournament.id]} onChange={(event) => {
+        dispatch(setLocalTimes({
+          tournamentId: tournament.id,
+          localTime: event.target.value
+        }))
+        if (tournamentsWebSocketRef.current) {
+          tournamentsWebSocketRef.current.send(JSON.stringify({
+            action: 'update',
+            tournament_id: tournament.id,
+            field: 'match_start_time',
+            value: new Date(event.target.value).toISOString()
+          }))
+        }
+      }}/>}
+      <Tooltip  id='addMatch'
+                border='1px solid red'><h3>Add Match</h3></Tooltip>
+        <button data-tooltip-id='addMatch' className={classes.addMatch} onClick={() => {
+          if (!matchesWebSocketsRef.current[tournament.id]) {
+            matchesWebSocketFunc(tournament);
+            setTimeout(() => {
+              matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+                action: 'create'
+              }))
+            }, 1000);
+          } else {
+            matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+              action: 'create'
+            }))
+          }
+        }}>
+          +
+        </button>
+    </div>
+    {matches[tournament.id] && playersInTeams && Object.keys(playersInTeams).length > 0 && Object.values(matches[tournament.id]).map((match: IMatch) => {
+      return <div key={match.id}>
+        {!matchEdit[match.id] && <div className={classes.matchContainer}>
+            <div className={classes.match}>
+            <h3 className={`${classes.player} ${classes.playerOne} ${match.winner === match.player_one ? classes.winner : ''}`}
+                onClick={() => {
+                  if (match.winner !== match.player_one) {
+                    matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+                      action: 'update',
+                      updated_field: match.id,
+                      updated_column: 'winner',
+                      updated_value: match.player_one
+                    }));
+                  }
+                }}>
+              {playersInTeams && playersInTeams[tournament.teamOne] && playersInTeams[tournament.teamOne][match.player_one]}
+              </h3>
+            <h3>VS</h3>
+            <h3 className={`${classes.player} ${match.winner === match.player_two ? classes.winner : ''}`}
+                onClick={() => {
+                  if (match.winner !== match.player_two) {
+                    matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+                      action: 'update',
+                      updated_field: match.id,
+                      updated_column: 'winner',
+                      updated_value: match.player_two
+                    }));
+                  }
+                }}>
+              {playersInTeams[tournament.teamTwo] && playersInTeams[tournament.teamTwo][match.player_two]}
+            </h3>
+          </div>
+          <div className={classes.matchInfo}>
+              <h3 className={classes.map}>{match.map !== null ? (match.map !== '' ? match.map : 'Map unspecified') : 'Map unspecified'}</h3>
+              <button className={classes.editButton} 
+                      onClick={() => {
+                        dispatch(setMatchEdit({
+                          matchId: match.id,
+                          edit: true
+                        }))
+                      }
+              }>
+                <img className={classes.edit} src={editWhite} alt="edit" draggable="false"/>
+              </button>
+              {!deletedMatches.includes(match.id) && <button className={classes.deleteButton} onClick={() => {
+                setDeletedMatches([...deletedMatches, match.id]);
+                matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+                  action: 'delete',
+                  match_pk: match.id
+                }));
+              }}>
+                <img className={classes.delete} src={deleteWhite} alt="delete" draggable="false"/>
+              </button>}
+              {deletedMatches.includes(match.id) && <span className={classes.loader}></span>}
+          </div>
+        </div>}
+        {matchEdit[match.id] && <div className={classes.matchContainer}>
+          <div className={classes.match}>
+            <select className={classes.select} value={match.player_one !== null ? match.player_one : 0} onChange={(event) => {
+              matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+                action: 'update',
+                updated_field: match.id,
+                updated_column: 'player_one',
+                updated_value: event.target.value
+              }))
+            }}>
+              <option className={classes.option} value="0" disabled>Select player</option>
+              {playersInTeams[tournament.teamOne] && Object.keys(playersInTeams[tournament.teamOne]).map((playerId) => {
+                return <option className={classes.option} key={playerId} value={playerId}>{playersInTeams[tournament.teamOne][parseInt(playerId)]}</option>
+              })}
+            </select>
+            <h3>VS</h3>
+            <select className={classes.select} value={match.player_two !== null ? match.player_two : 0} onChange={(event) => {
+              matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+                action: 'update',
+                updated_field: match.id,
+                updated_column: 'player_two',
+                updated_value: event.target.value
+              }));
+            }}>
+              <option className={classes.option} value="0" disabled>Select player</option>
+              {playersInTeams[tournament.teamTwo] && Object.keys(playersInTeams[tournament.teamTwo]).map((playerId) => {
+                return <option className={classes.option} key={playerId} value={playerId}>{playersInTeams[tournament.teamTwo][parseInt(playerId)]}</option>
+              })}
+            </select>
+          </div>
+          <div className={classes.matchInfo}>
+            <input placeholder='MAP' className={classes.mapNames} type="text" value={mapNames[match.id]} onChange={(event) => {
+              dispatch(setMapNames({
+                matchId: match.id,
+                mapNames: event.target.value
+              }))
+              matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+                action: 'update',
+                updated_field: match.id,
+                updated_column: 'map',
+                updated_value: event.target.value
+              }));
+            }} />
+            <button className={classes.editButton} 
+                    onClick={() => {
+                      dispatch(setMatchEdit({
+                        matchId: match.id,
+                        edit: false
+                      }))
+                    }
+              }>
+                <img className={classes.edit} src={editWhite} alt="edit" draggable="false"/>
+              </button>
+              {!deletedMatches.includes(match.id) && <button className={classes.deleteButton} onClick={() => {
+                setDeletedMatches([...deletedMatches, match.id]);
+                matchesWebSocketsRef.current[tournament.id].send(JSON.stringify({
+                  action: 'delete',
+                  match_pk: match.id
+                }));
+              }}>
+                <img className={classes.delete} src={deleteWhite} alt="delete" draggable="false"/>
+              </button>}
+              {deletedMatches.includes(match.id) && <span className={classes.loader}></span>}
+          </div>
+        </div>}
+      </div>
+    })}
+    <div className={classes.isFinished}>
+      <label htmlFor={`isFinished_${tournament.id}`} className={classes.isFinishedLabel}>Completed</label>
+      <input id={`isFinished_${tournament.id}`} type="checkbox" checked={tournament.isFinished} onChange={() => {
+        tournamentsWebSocketRef.current?.send(JSON.stringify({
+          action: "update",
+          tournament_id: tournament.id,
+          field: "is_finished",
+          value: !tournament.isFinished
+        }))
+      }}/>
+    </div>
+  </div>
+}
+
+export default GameAProgress
