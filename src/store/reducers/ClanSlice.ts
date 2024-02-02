43d37f25c5dc47ba02092db86b004ad28409a245
EllectronChik <electronchik04@mail.ru>
2024-02-02 22:51:05 +0300

    added players at clan slice

--- src/store/reducers/ClanSlice.ts
+++ src/store/reducers/ClanSlice.ts
@@ -1,18 +1,21 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IClan } from "../../models/IClan";
 import { RootState } from "../store";
+import { IPlayer } from "models/IPlayer";
 
 
 interface ClanState {
     clan: IClan | null,
     error: string | null,
     isLoading: boolean,
+    players: IPlayer[]
 }
 
 const initialState: ClanState = {
     clan: null,
     error: null,
     isLoading: false,
+    players: []
 }
 
 const clanSlice = createSlice({
@@ -33,16 +36,32 @@ const clanSlice = createSlice({
                 const { field, value } = action.payload;
                 (state.clan[field] as typeof state.clan[keyof IClan]) = value;
             };
+        },
+        setPlayers: (state, action: PayloadAction<IPlayer[]>) => {
+            state.players = action.payload
+        },
+        setPlayerWins: (state, action: PayloadAction<{ playerWins: number, index: number}>) => {
+            const trueIndex = state.players.findIndex((p) => p.id === action.payload.index)
+            if (trueIndex !== -1) {
+                state.players[trueIndex].wins = action.payload.playerWins
+            }
+        },
+        setPlayerGames: (state, action: PayloadAction<{ playerGames: number, index: number}>) => {
+            const trueIndex = state.players.findIndex((p) => p.id === action.payload.index)
+            if (trueIndex !== -1) {
+                state.players[trueIndex].total_games = action.payload.playerGames
+            }
         }
 
     }
 })
 
 
-export const { setClan, setLoading, updateClanField, setError } = clanSlice.actions;
+export const { setClan, setLoading, updateClanField, setError, setPlayers, setPlayerGames, setPlayerWins } = clanSlice.actions;
 
 export const selectClan = (state: RootState) => state.clan.clan;
 export const selectLoading = (state: RootState) => state.clan.isLoading;
 export const selectError = (state: RootState) => state.clan.error;
+export const selectPlayers = (state: RootState) => state.clan.players;
 
 export default clanSlice.reducer;
