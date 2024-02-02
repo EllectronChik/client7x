df6ea0caac0e677f45565d1c1ccbd57aca38fddd
EllectronChik <electronchik04@mail.ru>
2024-02-02 22:48:56 +0300

    moved startPage to folder

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,4 @@
-import StartPage from '../pages/StartPage';
+import StartPage from '../pages/StartPage/StartPage';
 import Arhive from './../pages/Arhive';
 import Login from '../pages/Login/Login';
 import Account from './../pages/Account/Account';
