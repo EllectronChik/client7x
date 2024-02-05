c38b1b10daa49b8df7093984a670ce6a6218e040
EllectronChik <electronchik04@mail.ru>
2024-02-05 23:48:40 +0300

    renamaed playerLogoService

--- src/services/PlayerLogoService.ts
+++ src/services/PlayerLogoService.ts
@@ -1,18 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
+const playerLogoApiUrl = import.meta.env.VITE_API_URL;
+
+const controller = new AbortController();
+const { signal } = controller;
 
 export const PlayerLogoApi = createApi({
     reducerPath: "playerLogoApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_API_URL,
+        baseUrl: playerLogoApiUrl,
     }),
     tagTypes: ["playerLogo"],
     endpoints: (builder) => ({
         fetchPlayerLogo: builder.query<string, { region: number, realm: number, id: number }>({
-            query: ({region, realm, id}) => ({
+            query: ({ region, realm, id }) => ({
                 url: `/get_player_logo/${region}/${realm}/${id}/`,
                 method: "GET",
+                signal
             })
         })
     })
-})
+});
+
+export const cancelAllLogoRequests = () => controller.abort();
