e7826d5ad43a15bff14de44b8ed59a45442f9c9d
EllectronChik <electronchik04@mail.ru>
2024-02-07 01:13:25 +0300

    new interface

---
+++ src/models/IInfoTours.ts
@@ -0,0 +1,18 @@
+interface IInfoTours {
+    groups: {
+      [key: string]: {
+        [key: string]: number
+      }
+    },
+    playoff: {
+      [key: number]: {
+        [key: number]: {
+          teamOne: string,
+          teamTwo: string,
+          teamOneWins: number,
+          teamTwoWins: number,
+          winner: string | null
+        }
+      }
+    },
+  }
