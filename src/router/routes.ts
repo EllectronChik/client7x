c667dfef883394fd2b69dc793f7cead5e9c7c4d5
EllectronChik <electronchik04@mail.ru>
2024-02-08 02:21:42 +0300

    Refactor: Reorganized public routes in index.js for clarity and consistency

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,5 @@
 import StartPage from "../pages/StartPage/StartPage";
-import Arhive from "./../pages/Arhive";
+import Arhive from "../pages/Arhive/Arhive";
 import Login from "../pages/Login/Login";
 import Account from "./../pages/Account/Account";
 
