4317f22481e215ea8bbb90b68f3d326ff12a7eb7
EllectronChik <electronchik04@mail.ru>
2024-02-09 01:50:20 +0300

    Refactor route paths and add Season page.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,13 @@
 import StartPage from "../pages/StartPage/StartPage";
-import Arhive from "../pages/Arhive/Arhive";
+import Archive from "../pages/Archive/Archive";
 import Login from "../pages/Login/Login";
 import Account from "./../pages/Account/Account";
+import Season from "pages/Season/Season";
 
 export const PublicRoutes = [
-  { path: "/arhive", Element: Arhive },
+  { path: "/archive", Element: Archive },
   { path: "/login", Element: Login },
   { path: "/account", Element: Account },
+  { path: "/season/:season/", Element: Season },
   { path: "/", Element: StartPage },
 ];
