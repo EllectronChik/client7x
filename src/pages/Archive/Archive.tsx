e58176024e81507f7a8df114755a009196996749
EllectronChik <electronchik04@mail.ru>
2024-02-11 16:22:15 +0300

    destructurize

--- src/pages/Archive/Archive.tsx
+++ src/pages/Archive/Archive.tsx
@@ -1,38 +1,9 @@
-import { useEffect, FC } from "react";
-import {
-  setFetching,
-  selectNextPage,
-} from "store/reducers/ArchiveSeasonsSlice";
-import { useAppDispatch, useAppSelector } from "hooks/reduxHooks";
-
+import { FC } from "react";
 import classes from "./Archive.module.scss";
 import ArchiveSeasons from "components/Archive/Seasons/ArchiveSeasons";
-import { useIntl } from "react-intl";
 import ArchiveTeams from "components/Archive/Teams/ArchiveTeams";
 
 const Archive: FC = () => {
-  const nextPage = useAppSelector(selectNextPage);
-  const dispatch = useAppDispatch();
-  const intl = useIntl();
-
-  const scrollHandler = () => {
-    if (
-      document.documentElement.scrollHeight -
-        (document.documentElement.scrollTop + window.innerHeight) <
-        100 &&
-      nextPage
-    ) {
-      dispatch(setFetching(true));
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("scroll", scrollHandler);
-    document.title = intl.formatMessage({ id: "archive" });
-
-    return () => document.removeEventListener("scroll", scrollHandler);
-  }, [nextPage]);
-
   return (
     <div className={classes.container}>
       <ArchiveSeasons />
