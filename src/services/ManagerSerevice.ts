0b6ee4a0c7c0134da8f311977ac116d6082a16e5
EllectronChik <electronchik04@mail.ru>
2024-02-14 15:01:29 +0300

    new endpoints

--- src/services/ManagerSerevice.ts
+++ src/services/ManagerSerevice.ts
@@ -18,5 +18,59 @@ export const ManagerApi = createApi({
         },
       }),
     }),
+    postManagerContacts: builder.mutation<
+      void,
+      { token: string; urls: string[] }
+    >({
+      query: ({ token, urls }) => ({
+        url: `/postManagerContacts/`,
+        method: "POST",
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+        body: {
+          urls: urls,
+        },
+      }),
+    }),
+    patchManagerContact: builder.mutation<
+      void,
+      { token: string; id: number; data: string }
+    >({
+      query: ({ token, id, data }) => ({
+        url: `/patchManagerContact/`,
+        method: "PATCH",
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+        body: {
+          id: id,
+          data: data,
+        },
+      }),
+    }),
+    deleteManagerContact: builder.mutation<void, { token: string; id: number }>(
+      {
+        query: ({ token, id }) => ({
+          url: `/deleteManagerContact/`,
+          method: "DELETE",
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+          body: {
+            id: id,
+          },
+        }),
+      }
+    ),
+    postManagerContact: builder.mutation<{ id: number }, { token: string }>({
+      query: ({ token }) => ({
+        url: `/postManagerContact/`,
+        method: "POST",
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      }),
+    }),
   }),
 });
