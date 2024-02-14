3fc0b0e4303895e30db5e935317d50612f0b7201
EllectronChik <electronchik04@mail.ru>
2024-02-14 14:58:35 +0300

    edit case style

--- src/models/IClanByManager.ts
+++ src/models/IClanByManager.ts
@@ -1,13 +1,23 @@
 import { IPlayer } from "./IPlayer";
 
 export interface IClanByManager {
-  team_id: number;
-  team_name: string;
-  team_logo_url: string;
-  team_tag: string;
-  team_region_name: string;
-  team_region_flag: string;
+  teamId: number;
+  teamName: string;
+  teamLogoUrl: string;
+  teamTag: string;
+  teamRegionName: string;
+  teamRegionFlag: string;
   players: IPlayer[];
-  team_resources?: string[];
-  is_reg_to_current_season?: boolean;
+  teamResources?: {
+    id: number;
+    teamId: number;
+    url: string;
+    name: string;
+  }[];
+  managerResources?: {
+    id: number;
+    userId: number;
+    url: string;
+  }[];
+  isRegToCurrentSeason?: boolean;
 }
