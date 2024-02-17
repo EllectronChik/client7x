004b5bfaeca453fffd7ba757efb6171528ba28e4
EllectronChik <electronchik04@mail.ru>
2024-02-17 17:28:19 +0300

    changed server to work on other devices

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  server: {
+    host: '0.0.0.0', // Разрешить подключения со всех IP-адресов
+    port: 5173,
+  },
 })
