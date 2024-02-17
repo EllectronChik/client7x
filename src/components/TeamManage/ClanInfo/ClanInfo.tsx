d3b18c1653a8cb4f78ea0b0768a722bc70dcc3e5
EllectronChik <electronchik04@mail.ru>
2024-02-17 18:35:55 +0300

    del excessive div

--- src/components/TeamManage/ClanInfo/ClanInfo.tsx
+++ src/components/TeamManage/ClanInfo/ClanInfo.tsx
@@ -57,148 +57,144 @@ const ClanInfo: FC = () => {
   };
 
   return (
-    <div>
-      <div className={classes.teamManage}>
-        {myTeam && (
-          <div className={classes.teamInfo}>
-            <div
-              className={classes.teamLogoBox}
-              onClick={() => logoInputRef.current?.click()}
-            >
-              <img
-                src={teamDefault}
-                alt={myTeam.teamName}
-                className={classes.teamLogo}
-                onLoad={(e) => {
-                  e.currentTarget.src = teamLogoUrl ? teamLogoUrl : teamDefault;
-                }}
-              />
-              <input
-                ref={logoInputRef}
-                type="file"
-                className={classes.editLogo}
-                onChange={(e) => {
-                  changeLogo({
+    <div className={classes.teamManage}>
+      {myTeam && (
+        <div className={classes.teamInfo}>
+          <div
+            className={classes.teamLogoBox}
+            onClick={() => logoInputRef.current?.click()}
+          >
+            <img
+              src={teamDefault}
+              alt={myTeam.teamName}
+              className={classes.teamLogo}
+              onLoad={(e) => {
+                e.currentTarget.src = teamLogoUrl ? teamLogoUrl : teamDefault;
+              }}
+            />
+            <input
+              ref={logoInputRef}
+              type="file"
+              className={classes.editLogo}
+              onChange={(e) => {
+                changeLogo({
+                  teamId: myTeam.teamId,
+                  logo: e.target.files![0],
+                  token: cookies.token,
+                });
+                setTeamLogoUrl(URL.createObjectURL(e.target.files![0]));
+              }}
+            />
+          </div>
+          <div>
+            <input
+              className={classes.teamName}
+              defaultValue={myTeam.teamName}
+              onChange={(e) => {
+                if (changeNameTimeout) {
+                  clearTimeout(changeNameTimeout);
+                }
+                changeNameTimeout = setTimeout(() => {
+                  changeName({
                     teamId: myTeam.teamId,
-                    logo: e.target.files![0],
+                    name: e.target.value,
                     token: cookies.token,
                   });
-                  setTeamLogoUrl(URL.createObjectURL(e.target.files![0]));
-                }}
-              />
-            </div>
+                }, 1000);
+              }}
+            />
             <div>
+              <span>&lt;</span>
               <input
-                className={classes.teamName}
-                defaultValue={myTeam.teamName}
+                ref={tagInputRef}
+                className={classes.teamTag}
+                defaultValue={myTeam.teamTag}
                 onChange={(e) => {
-                  if (changeNameTimeout) {
-                    clearTimeout(changeNameTimeout);
+                  if (changeTagTimeout) {
+                    clearTimeout(changeTagTimeout);
                   }
-                  changeNameTimeout = setTimeout(() => {
-                    changeName({
+                  changeTagTimeout = setTimeout(() => {
+                    changeTag({
                       teamId: myTeam.teamId,
-                      name: e.target.value,
+                      tag: e.target.value,
                       token: cookies.token,
                     });
                   }, 1000);
+                  if (tagInputRef.current) {
+                    const textWidth = getTextWidth(e.target.value);
+                    tagInputRef.current.style.width = `${textWidth}px`;
+                  }
                 }}
               />
-              <div>
-                <span>&lt;</span>
-                <input
-                  ref={tagInputRef}
-                  className={classes.teamTag}
-                  defaultValue={myTeam.teamTag}
-                  onChange={(e) => {
-                    if (changeTagTimeout) {
-                      clearTimeout(changeTagTimeout);
-                    }
-                    changeTagTimeout = setTimeout(() => {
-                      changeTag({
-                        teamId: myTeam.teamId,
-                        tag: e.target.value,
-                        token: cookies.token,
-                      });
-                    }, 1000);
-                    if (tagInputRef.current) {
-                      const textWidth = getTextWidth(e.target.value);
-                      tagInputRef.current.style.width = `${textWidth}px`;
-                    }
-                  }}
-                />
-                <span>&gt;</span>
-              </div>
+              <span>&gt;</span>
             </div>
-            <div className={classes.regionFlag}>
-              <img
-                draggable={false}
-                src={`${import.meta.env.VITE_SERVER_URL}${
-                  myTeam.teamRegionFlag
+          </div>
+          <div className={classes.regionFlag}>
+            <img
+              draggable={false}
+              src={`${import.meta.env.VITE_SERVER_URL}${myTeam.teamRegionFlag}`}
+              alt={myTeam.teamRegionName}
+            />
+          </div>
+        </div>
+      )}
+      <div className={classes.teamContent}>
+        <div className={classes.playersInfo}>
+          {teamRegistred && (
+            <h3 className={classes.playersInfoTitle}>
+              <FormattedMessage id="clickOnPlayerToAdd" />
+            </h3>
+          )}
+          {players &&
+            players.map((player, index) => (
+              <InClanPlayerBox
+                className={`${
+                  regPlayers.find((p) => p.id === player.id)
+                    ? ""
+                    : classes.playerNotRegistred
                 }`}
-                alt={myTeam.teamRegionName}
-              />
-            </div>
-          </div>
-        )}
-        <div className={classes.teamContent}>
-          <div className={classes.playersInfo}>
-            {teamRegistred && (
-              <h3 className={classes.playersInfoTitle}>
-                <FormattedMessage id="clickOnPlayerToAdd" />
-              </h3>
-            )}
-            {players &&
-              players.map((player, index) => (
-                <InClanPlayerBox
-                  className={`${
-                    regPlayers.find((p) => p.id === player.id)
-                      ? ""
-                      : classes.playerNotRegistred
-                  }`}
-                  key={index}
-                  player={player}
-                  onClick={() => {
-                    {
-                      if (
-                        teamRegistred &&
-                        !regPlayers.find((p) => p.id === player.id)
-                      ) {
-                        setPlayerToSeason({
-                          player_id: player.id,
-                          token: cookies.token,
-                          season: currentTournament?.number ?? 0,
-                        });
-                        dispatch(addRegPlayer(player));
-                      }
+                key={index}
+                player={player}
+                onClick={() => {
+                  {
+                    if (
+                      teamRegistred &&
+                      !regPlayers.find((p) => p.id === player.id)
+                    ) {
+                      setPlayerToSeason({
+                        player_id: player.id,
+                        token: cookies.token,
+                        season: currentTournament?.number ?? 0,
+                      });
+                      dispatch(addRegPlayer(player));
                     }
-                  }}
-                />
-              ))}
-            <div className={classes.manageBtns}>
-              <Button7x
-                className={classes.manageBtn}
-                onClick={() => setManagePage(managePage === 1 ? 0 : 1)}
-              >
-                <FormattedMessage id="addPlayers" />
-              </Button7x>
-              <Button7x
-                className={classes.manageBtn}
-                onClick={() => setManagePage(managePage === 2 ? 0 : 2)}
-              >
-                <FormattedMessage id="editLinks" />
-              </Button7x>
-            </div>
-            {managePage === 1 && myTeam && myTeam.teamTag && (
-              <AddPlayers clantag={myTeam.teamTag} />
-            )}
-            {managePage === 2 && myTeam && (
-              <EditLinks
-                managerResources={myTeam.managerResources}
-                teamResources={myTeam.teamResources}
+                  }
+                }}
               />
-            )}
+            ))}
+          <div className={classes.manageBtns}>
+            <Button7x
+              className={classes.manageBtn}
+              onClick={() => setManagePage(managePage === 1 ? 0 : 1)}
+            >
+              <FormattedMessage id="addPlayers" />
+            </Button7x>
+            <Button7x
+              className={classes.manageBtn}
+              onClick={() => setManagePage(managePage === 2 ? 0 : 2)}
+            >
+              <FormattedMessage id="editLinks" />
+            </Button7x>
           </div>
+          {managePage === 1 && myTeam && myTeam.teamTag && (
+            <AddPlayers clantag={myTeam.teamTag} />
+          )}
+          {managePage === 2 && myTeam && (
+            <EditLinks
+              managerResources={myTeam.managerResources}
+              teamResources={myTeam.teamResources}
+            />
+          )}
         </div>
       </div>
     </div>
