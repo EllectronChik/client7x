b1323b719eaeb984cd8a3301c74f69bfcee24c00
EllectronChik <electronchik04@mail.ru>
2024-02-25 20:55:33 +0300

    added compressionPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
+import compressionPlugin from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    compressionPlugin({
+      ext: ".gz",
+    }),
+  ],
   server: {
-    host: '0.0.0.0', // Разрешить подключения со всех IP-адресов
+    host: "0.0.0.0",
     port: 5173,
   },
   resolve: {
     alias: {
-      '@settings.scss': './src/settings.scss',
-    }
-  }
-})
+      "@settings.scss": "./src/settings.scss",
+    },
+  },
+});
