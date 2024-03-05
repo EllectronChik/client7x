b19ffc3a4413d2347511812cc8b4eefbea155e64
EllectronChik <electronchik04@mail.ru>
2024-03-05 14:10:08 +0300

    changed the order of inputs

--- src/components/ClanUtils/MediaForm.tsx
+++ src/components/ClanUtils/MediaForm.tsx
@@ -22,6 +22,28 @@ export const handleAddMediaForm = (
     <div className={classes.mediaForm} key={newId}>
       <div className={classes.mediaFormBox}>
         <label>
+          <FormattedMessage id="media" /> {resForms.length + 1} name:
+        </label>
+        <Input7x
+          type="text"
+          onChange={(e) => {
+            const newValue = e.target.value;
+            setResorces((resorces) => {
+              const updatedResources = [...resorces];
+              if (!updatedResources[newId]) {
+                updatedResources[newId] = {
+                  id: newId,
+                  url: "",
+                  name: newValue,
+                };
+              } else {
+                updatedResources[newId].name = newValue;
+              }
+              return updatedResources;
+            });
+          }}
+        />
+        <label>
           <FormattedMessage id="media" /> {resForms.length + 1} url:
         </label>
         <Input7x
@@ -39,28 +61,6 @@ export const handleAddMediaForm = (
                 };
               } else {
                 updatedResources[newId].url = newValue;
-              }
-              return updatedResources;
-            });
-          }}
-        />
-        <label>
-          <FormattedMessage id="media" /> {resForms.length + 1} name:
-        </label>
-        <Input7x
-          type="text"
-          onChange={(e) => {
-            const newValue = e.target.value;
-            setResorces((resorces) => {
-              const updatedResources = [...resorces];
-              if (!updatedResources[newId]) {
-                updatedResources[newId] = {
-                  id: newId,
-                  url: "",
-                  name: newValue,
-                };
-              } else {
-                updatedResources[newId].name = newValue;
               }
               return updatedResources;
             });
