77a89e057ac6bb868434990275a50425924edc40
EllectronChik <electronchik04@mail.ru>
2024-03-05 14:07:03 +0300

    add firstLoad state

--- src/store/reducers/pageManagerSlice.ts
+++ src/store/reducers/pageManagerSlice.ts
@@ -3,10 +3,12 @@ import { RootState } from "../store";
 
 interface PageManagerState {
   page: number | null;
+  firstLoad: boolean;
 }
 
 const initialState: PageManagerState = {
   page: null,
+  firstLoad: true,
 };
 
 const pageManagerSlice = createSlice({
@@ -16,10 +18,15 @@ const pageManagerSlice = createSlice({
     setPageManager: (state, action) => {
       state.page = action.payload;
     },
+    setFirstLoad: (state, action) => {
+      state.firstLoad = action.payload;
+    },
   },
 });
 
 export const selectManagerPage = (state: RootState) => state.pageManager.page;
+export const selectManagerFirstLoad = (state: RootState) =>
+  state.pageManager.firstLoad;
 
-export const { setPageManager } = pageManagerSlice.actions;
+export const { setPageManager, setFirstLoad } = pageManagerSlice.actions;
 export default pageManagerSlice.reducer;
